d79b12da1ef5a3761938779e564e506e3e0df1fa
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2024-02-20 14:27:50 +0000

    Corrected download cv text size and colour

--- src/Components/ContactAndCv.tsx
+++ src/Components/ContactAndCv.tsx
@@ -2,7 +2,7 @@ export default function ContactAndCv() {
   return (
     <>
       <div className="w-fit h-fit flex space-x-3">
-        <span className="my-auto text-[1rem] xs:hidden sm:block text-white hover:text-[#cdcbcb] cursor-pointer">
+        <span className="my-auto text-sm xs:hidden sm:block text-[#878686] hover:text-white cursor-pointer">
           Download CV
         </span>
         <span className="px-4 py-2 bg-[#21551b] xs:text-[1.25rem] sm:text-[1rem] text-white rounded-full hover:text-[#cdcbcb] hover:brightness-90 cursor-pointer">
