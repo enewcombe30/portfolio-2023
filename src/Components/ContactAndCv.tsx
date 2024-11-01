081a52aefd81bbbf67058a6998bbf5341a394d62
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2024-11-01 14:35:44 +0000

    Title and fav changes

--- src/Components/ContactAndCv.tsx
+++ src/Components/ContactAndCv.tsx
@@ -1,9 +1,16 @@
 export default function ContactAndCv() {
+  const showCV = () => {
+    window.open("../Components/pdfs/ElijahCV2023.pdf");
+  };
+
   return (
     <>
       <div className="w-fit h-fit flex space-x-3">
-        <span className="my-auto text-sm xs:hidden sm:block text-[#878686] hover:text-white cursor-pointer">
-          Download CV
+        <span
+          onClick={showCV}
+          className="my-auto text-sm xs:hidden sm:block text-[#878686] hover:text-white cursor-pointer"
+        >
+          View CV
         </span>
         <span className="px-4 py-2 bg-[#21551b] xs:text-[1.25rem] sm:text-[1rem] text-white rounded-full hover:text-[#cdcbcb] hover:brightness-90 cursor-pointer">
           Get in touch
