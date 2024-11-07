797caac56631ef1cfe25e11c340ec803bcf4058c
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2024-11-07 08:35:45 +0000

    updated experience

--- src/Pages/Stack/constants/stack.ts
+++ src/Pages/Stack/constants/stack.ts
@@ -18,7 +18,7 @@ export const TechStack: Stack = {
       description:
         "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser",
       commercialExp: "1 year Commercial experience",
-      personalExp: "2 years Personal project experience",
+      personalExp: "3 years Personal project experience",
     },
     {
       name: "css",
@@ -27,7 +27,7 @@ export const TechStack: Stack = {
       description:
         "Cascading Style Sheets (CSS) is a style sheet language used for specifying the presentation and styling of a document written in a markup language such as HTML or XML (including XML dialects such as SVG, MathML or XHTML)",
       commercialExp: "1 year Commercial experience",
-      personalExp: "2 years Personal project experience",
+      personalExp: "3 years Personal project experience",
     },
     {
       name: "js",
@@ -36,7 +36,7 @@ export const TechStack: Stack = {
       description:
         "Often abbreviated as JS, is a programming language and core technology of the World Wide Web, alongside HTML and CSS. As of 2024, 98.9% of websites use JavaScript on the client side for webpage behavior",
       commercialExp: "1 year Commercial experience",
-      personalExp: "2 years Personal project experience",
+      personalExp: "3 years Personal project experience",
     },
     {
       name: "github",
@@ -45,7 +45,7 @@ export const TechStack: Stack = {
       description:
         "GitHub is a developer platform that allows developers to create, store, manage and share their code.",
       commercialExp: "1 year Commercial experience",
-      personalExp: "2 years Personal project experience",
+      personalExp: "3 years Personal project experience",
     },
     {
       name: "nodejs",
@@ -63,7 +63,7 @@ export const TechStack: Stack = {
       description:
         "WordPress (also known as WP or WordPress.org) is a web content management system. It was originally created as a tool to publish blogs but has evolved to support publishing other web content, including more traditional websites, mailing lists and Internet forum, media galleries, membership sites, learning management systems and online stores.",
       commercialExp: "1 year Commercial experience",
-      personalExp: null,
+      personalExp: "3 years Personal project experience",
     },
     {
       name: "react",
@@ -72,7 +72,7 @@ export const TechStack: Stack = {
       description:
         "React (also known as React.js or ReactJS) is a free and open-source front-end JavaScript library for building user interfaces based on components.",
       commercialExp: "1 year Commercial experience",
-      personalExp: "1 years Personal project experience",
+      personalExp: "2 years Personal project experience",
     },
     {
       name: "ts",
@@ -81,7 +81,7 @@ export const TechStack: Stack = {
       description:
         "TypeScript is a free and open-source high-level programming language developed by Microsoft that adds static typing with optional type annotations to JavaScript. It is designed for the development of large applications and transpiles to JavaScript",
       commercialExp: "1 year Commercial experience",
-      personalExp: "1 years Personal project experience",
+      personalExp: "2 years Personal project experience",
     },
     {
       name: "tailwind",
@@ -89,7 +89,7 @@ export const TechStack: Stack = {
       logo: TailwindLogo(),
       description: `Tailwind CSS is an open source CSS framework. The main feature of this library is that, unlike other CSS frameworks like Bootstrap, it does not provide a series of predefined classes for elements such as buttons or tables. Instead, it creates a list of "utility" CSS classes that can be used to style each element by mixing and matching`,
       commercialExp: "1 year Commercial experience",
-      personalExp: "1 years Personal project experience",
+      personalExp: "2 years Personal project experience",
     },
   ],
 };
