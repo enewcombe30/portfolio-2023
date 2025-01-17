33f8d1052298945736ed947d4f485e49b587f00b
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2025-01-17 09:43:27 +0000

    added pipes

--- src/Pages/Welcome/Welcome.tsx
+++ src/Pages/Welcome/Welcome.tsx
@@ -8,8 +8,10 @@ export default function WelcomePage() {
         <div className="xs:text-[2.25rem] sm:text-[2.25rem] md:text-[3rem] lg:text-[4.5rem] my-auto text-green-600">
           Frontend Web Developer
         </div>
-        <div className="xs:text-[0.825rem] sm:text-[1rem] md:text-[1.25rem] lg:text-[1.1] md:my-10 sm:my-6 xs:my-8">
-          Javascript | Typescript | TailwindCSS | React
+        <div className="xs:text-[2rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3rem] md:my-10 sm:my-6 xs:my-8">
+          Javascript<span className="text-green-600"> | </span>Typescript
+          <span className="text-green-600"> | </span>TailwindCSS
+          <span className="text-green-600"> | </span>React
         </div>
       </div>
     </div>
