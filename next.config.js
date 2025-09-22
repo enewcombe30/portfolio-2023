ba084a47d6de2ab0f226981955d9ebb0422d2c2e
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2025-09-22 22:54:25 +0100

    next config fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/portfolio-2023",
+  assetPrefix: "/portfolio-2023/",
 };
 
 module.exports = nextConfig;
