a54bf61b93f5ee8017b1beac7e7914fdbfd7eb95
elijah newcombe <78599237+enewcombe30@users.noreply.github.com>
2025-09-23 13:36:30 +0100

    styling changes for publish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    // "./src/pages/**/*.{js,ts,jsx,tsx}",
-    // "./src/Components/**/*.{js,ts,jsx,tsx}",
-    // "./src/PageComponents/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       xs: "200px",
